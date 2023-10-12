731456de23abce34ff73217f08c8e603f9b33950
neotheicebird <neotheicebird@gmail.com>
2023-10-12 22:51:19 +0530

    remove spacing

--- Formula/d/dns-scout.rb
+++ Formula/d/dns-scout.rb
@@ -3,8 +3,7 @@ class DnsScout < Formula
     homepage "https://github.com/careyjames/DNS-Scout"
     url "https://github.com/careyjames/DNS-Scout/archive/refs/tags/v5.8.tar.gz"
     sha256 "2ac687dfdd7f218a86ae5235c05f63c785af00786366e26f90d600972a5b282a"
-    license "MIT"
-    
+    license "MIT"  
     depends_on "go" => :build
   
     def install
